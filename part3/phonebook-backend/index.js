8eb2b78465d7233179547a4e27928eeca17cc16b
Pavlo Bilousov <bilousov.p@icloud.com>
2026-02-23 21:16:59 +0100

    [P3.a] Complete excecises 3.7 - 3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+
+morgan.token('post-data', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
 let persons = [
     { 

7bcf6c2ec0eedb7a199ab4a2633cc901fb46b2e9
Pavlo Bilousov <bilousov.p@icloud.com>
2026-02-27 01:15:38 +0100

    [P3.c] Complete excecise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const nameInput = process.argv[3]
+const numberInput = process.argv[4]
+
+const url = `mongodb+srv://db_access:${password}@cluster0.xrhjica.mongodb.net/phonebookApp?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+const Person = mongoose.model('Person', personSchema)
+
+
+if (nameInput && numberInput) {
+  console.log("Arguments for create specified. Creating...");
+  
+  const person = new Person({
+    name: nameInput,
+    number: numberInput
+  })
+
+  person.save().then(result => {
+    console.log(`Added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close()
+  })
+} else {
+  console.log("No args. Listing phonebook...")
+
+  Person.find({}).then(result => {
+    console.log("phonebook:");
+    
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`); 
+    })
+
+    mongoose.connection.close()
+  })
+}

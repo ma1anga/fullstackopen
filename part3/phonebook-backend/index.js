402888dd161a9883fc504bbb573f9cea1743d55a
Pavlo Bilousov <bilousov.p@icloud.com>
2026-02-28 22:48:36 +0100

    [P3.c] Complete excecises 3.15 - 3.18

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -13,12 +13,12 @@ app.use(express.static('dist'))
 morgan.token('post-data', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.number || !body.name) {
-    return response.status(400).json({ 
-      error: 'name and/or number field(s) missing' 
+    return response.status(400).json({
+      error: 'name and/or number field(s) missing'
     })
   }
 
@@ -29,38 +29,79 @@ app.post('/api/persons', (request, response) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  })
+  }).catch(error => { next(error) })
 })
 
-app.get('/api/persons', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+  const { number } = request.body
+  const { id } = request.params
+
+  Person.findById(id).then(personToUpdate => {
+    if (!personToUpdate) {
+      response.status(404).end()
+    }
+
+    personToUpdate.number = number
+
+    personToUpdate.save().then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+  }).catch(error => { next(error) })
+})
+
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons)
-  })
+  }).catch(error => { next(error) })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
-  
+
   Person.findById(id).then(person => {
-    response.json(person)
-  })
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => { next(error) })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+  const { id } = request.params
 
-  response.status(204).end()
+  Person.findByIdAndDelete(id).then(deletedPerson => {
+    if (deletedPerson) {
+      response.json(deletedPerson)
+    } else {
+      response.status(204).end()
+    }
+  }).catch(error => { next(error) })
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   const receivedDateTime = new Date();
 
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${receivedDateTime}</p>
+  Person.countDocuments().then(total => {
+    response.send(`
+      <p>Phonebook has info for ${total} people</p>
+      <p>${receivedDateTime}</p>
   `)
+  }).catch(error => { next(error) }) 
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// This has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {

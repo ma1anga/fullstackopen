f619db8a634c1eb8081ab9e2ade98d0a5beaf88b
Pavlo Bilousov <bilousov.p@icloud.com>
2026-03-05 22:08:23 +0100

    [P4.a] Complete excecises 4.1 - 4.2

---
+++ part4/blog-list-backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+logger.info('connecting to MongoDB')
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
